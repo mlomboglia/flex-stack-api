c519610e2ebf5da1f201d10464237ff0054a18eb
Marcos Lomboglia <marcos.lomboglia@gmail.com>
2019-10-28 16:34:54 +0000

    Setting up our Serverless infrastructure

---
+++ list.js
@@ -0,0 +1,30 @@
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+import { success, failure } from "./libs/response-lib";
+const TokenValidator = require('twilio-flex-token-validator').validator;
+
+export async function main(event, context) {
+
+    //console.log('Request Headers:', event.headers);
+    //console.log('Request Jwe:', event.headers['X-Flex-JWE']);
+    //console.log('Request Body:', event.body);
+
+    try {
+        const token = event.headers['X-Flex-JWE'];
+        const tokenResult = await TokenValidator(token, process.env.ACCOUNT_SID, process.env.AUTH_TOKEN);
+        console.log("validated token", tokenResult);
+
+        const params = {
+            TableName: process.env.tableName,
+            KeyConditionExpression: "account_sid = :account_sid",
+            ExpressionAttributeValues: {
+                ":account_sid": process.env.accountSid
+            }
+        };
+        const result = await dynamoDbLib.call("query", params);
+        return success(result.Items);
+
+    } catch (err) {
+        console.log(err);
+        return failure(err);
+    }
+}
